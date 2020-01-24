1f5b70993914663749d217ec3a66d0fdfc340152
Karen Zellers <karen.zellers@gmail.com>
2020-01-23 21:13:29 -0800

    commit project

---
+++ hello.rb
@@ -0,0 +1,8 @@
+def say_hello
+   puts "Hello! What is your name?"
+   input = gets.strip
+   puts "Hi #{input}!"
+end
+
+say_hello
+
---
+++ stock_picker.rb
@@ -0,0 +1,44 @@
+def pick_stocks(stocks)
+   stocks =  stocks[0]
+   max_profit = 0
+   min_stock = stocks[0]
+   index1_temp = 0
+   index1_lock = 0
+   index2 = 0
+   for i in 1...stocks.length()
+      if stocks[i] < min_stock
+         min_stock = stocks[i]
+         index1_temp = i
+      else
+         if max_profit < (stocks[i]-min_stock)
+            max_profit = stocks[i]-min_stock
+            index2 = i
+            index1_lock = index1_temp
+         end
+      end
+   end
+   if index1_lock == index2
+      puts "Not a good time to invest"
+   else
+      puts "I would recommend buying on Day #{index1_lock + 1} and selling on Day #{index2 + 1}."
+   end
+end
+
+
+puts "Enter the number of days to track"
+num = Integer(gets)
+stocks = []
+count = 1
+num.times do
+  if stocks == []
+     print "Enter the Day 1 stock value: "
+  else
+     print "Enter the Day #{count} stock value: "
+  end
+  s = Integer(gets.chomp)
+  stocks.push(s)
+  count += 1
+end
+   
+
+pick_stocks([stocks])
